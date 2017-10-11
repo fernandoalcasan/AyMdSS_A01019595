3dc0043c114d54d6c5a262e1ab4ebe157a7638a2
fernandoalcasan <fernandoalcasan@outlook.com>
2017-10-11 00:13:11 -0500

    Observer terminado

--- Actividades/Observer/Main.cpp
+++ Actividades/Observer/Main.cpp
@@ -6,82 +6,140 @@ using namespace std;
 
 class Observer
 {
-protected:
-	vector<Subject*> presidentes;
-};
-
-class Subject
-{
-private:
-	vector<Observer*> noticieros;
-	string accion;
-
 public:
-	string getAccion() { return accion; }
-	
-	void setAccion(string n) 
-	{
-		accion = n;
-	}
-
-	void agregarObserver(Observer* ob) 
-	{
-		noticieros.push_back(ob);
-	}
+	virtual void noticia(string na, string no) = 0;
 };
 
 class MVSNoticias : public Observer
 {
 public:
-	MVSNoticias(vector<Subject*> n)
+	MVSNoticias() = default;
+	void noticia(string na, string no)
 	{
-		presidentes = n;
+		cout << "MVS Noticias informa: " << na << " dijo que " << no << endl;
 	}
 };
 
 class TelevisaNoticias : public Observer
 {
 public:
-	TelevisaNoticias(vector<Subject*> n)
+	TelevisaNoticias() = default;
+	void noticia(string na, string no)
 	{
-		presidentes = n;
+		cout << "Televisa Noticias informa: " << na << " dijo que " << no << endl;
 	}
 };
 
 class RadioRealNoticias : public Observer
 {
 public:
-	RadioRealNoticias(vector<Subject*> n)
+	RadioRealNoticias() = default;
+	void noticia(string na, string no)
 	{
-		presidentes = n;
+		cout << "Radio Real Noticias informa: " << na << " dijo que " << no << endl;
 	}
 };
 
 class NoticiasInternacionales : public Observer
 {
 public:
-	NoticiasInternacionales(vector<Subject*> n)
+	NoticiasInternacionales() = default;
+	void noticia(string na, string no)
 	{
-		presidentes = n;
+		cout << "Noticias Internacionales informa: " << na << " dijo que " << no << endl;
 	}
 };
 
+
+class Subject
+{
+private:
+	vector<Observer*> noticieros;
+	
+protected:
+	string accion;
+	string name;
+
+public:
+	Subject() = default;
+	Subject(string a) : accion(a) {}
+	string getAccion() { return accion; }
+	string getNombre() { return name; }
+	
+	void setAccion(string a) 
+	{
+		accion = a;
+	}
+
+	void setNombre(string n)
+	{
+		name = n;
+	}
+
+	void agregarObserver(Observer* ob) 
+	{
+		noticieros.push_back(ob);
+	}
+
+	void Cajetearla(string c)
+	{
+		for (int i = 0; i < noticieros.size(); i++)
+		{
+			noticieros[i]->noticia(getNombre() , c);
+		}
+	}
+};
+
+
 class Trump : public Subject
 {
-
+public:
+	Trump()
+	{
+		name = "Donald Trompetas";
+	}
 };
 
 class Pena : public Subject
 {
-
+public:
+	Pena()
+	{
+		name = "Peña Nieto";
+	}
 };
 
 class KimJongUn : public Subject
 {
-
+public:
+	KimJongUn()
+	{
+		name = "Kim Jong-Un";
+	}
 };
 
 int main()
 {
+	Trump* trompetas = new Trump();
+	Pena* penaAjena = new Pena();
+	KimJongUn* kimLocoUn = new KimJongUn();
+	MVSNoticias* mvs = new MVSNoticias();
+	TelevisaNoticias* telenovelas = new TelevisaNoticias();
+	RadioRealNoticias* rrn = new RadioRealNoticias();
+	NoticiasInternacionales* ni = new NoticiasInternacionales();
 
+	trompetas->agregarObserver(mvs);
+	trompetas->agregarObserver(telenovelas);
+	trompetas->agregarObserver(rrn);
+	trompetas->agregarObserver(ni);
+
+	trompetas->Cajetearla("Pueeeeerto Reeeeeeeeeeco will rise!");
+
+	delete trompetas;
+	delete penaAjena;
+	delete kimLocoUn;
+	delete mvs;
+	delete telenovelas;
+	delete rrn;
+	delete ni;
 }
